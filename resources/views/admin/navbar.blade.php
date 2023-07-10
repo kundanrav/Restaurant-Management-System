801c60a8eabe610427de951cc95bebca84218bbd
kundanrav <krao8364@gmail.com>
2023-07-10 15:38:56 +0530

    add

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -14,7 +14,7 @@
     <ul class="nav">
       <li class="nav-item nav-category">Main</li>
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="#">
           <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
           <span class="menu-title">Dashboard</span>
         </a>
